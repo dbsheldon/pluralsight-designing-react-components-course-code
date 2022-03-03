ad3398964011d900d0fd48d8ffa3247e767f0e13
Dominic Sheldon <dsheldon@sgi.sk.ca>
2022-03-03 16:31:06 -0600

    Separate state management from context

--- myCode/contexts/ThemeContext.js
+++ myCode/contexts/ThemeContext.js
@@ -1,9 +1,10 @@
 import React, { createContext, useState } from "react";
+import useTheme from "../hooks/useTheme";
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ startingTheme, children }) {
-  const [theme, setTheme] = useState(startingTheme);
+  const { theme, setTheme } = useTheme(startingTheme);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
